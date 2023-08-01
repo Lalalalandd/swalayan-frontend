486c1dd1c2a2f4ce5c20024dd76fde7b903c2541
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2023-08-01 12:32:41 +0700

    swalayan frontend

---
+++ template/sidebar.php
@@ -0,0 +1,47 @@
+<!-- Sidebar -->
+<div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Anggap Saja Admin</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>Employee</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="product.php" class="nav-link">
+              <i class="nav-icon fas fa-file"></i>
+              <p>Product</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="incoming_product.php" class="nav-link">
+              <i class="nav-icon fas fa-file"></i>
+              <p>Incoming Product</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="transaction.php" class="nav-link">
+              <i class="nav-icon fas fa-file"></i>
+              <p>Transaction</p>
+            </a>
+          </li>
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
